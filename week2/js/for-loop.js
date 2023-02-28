91a4c183d2c8d3ddb24da77aebd256585ea65a18
John Rowley <outsidethebox@eircom.net>
2023-02-28 19:32:34 +0000

    Added loops

---
+++ week2/js/for-loop.js
@@ -0,0 +1,24 @@
+
+// let step = 0;
+// while(step < topStep) {
+
+//     console.log(`i am step ${step}`); 
+//     // step = step + 1;
+//     step++;
+
+// }
+
+let topStep = 10;
+for (let step = 0; step < topStep; step++) {
+
+    console.log(`i am step ${step}`);
+}
+
+function GoUpSteps(topStep) {
+
+    for (let step = 0; step < topStep; step++) {
+
+        console.log(`i am step ${step}`);
+    }
+
+}
---
+++ week2/js/list1.js
@@ -0,0 +1,14 @@
+// let fruit1 = "apple";
+// let fruit2 = "pear";
+// let fruit3 = "banana";
+
+// array - which is used stores lists 
+
+let fruit = []; // initialising the list or array - it will be used to store strings 
+fruit[0]="apple"; // [0] referred as the index position, which always starts at 0
+fruit[1]="pear"; // notice the use of square brackets for the index position
+fruit[2]="banana";
+
+console.log(`The first piece of fruit is ${fruit[0]}`);
+console.log(`The second piece of fruit is ${fruit[1]}`);
+console.log(`The third piece of fruit is ${fruit[2]}`); // notice the index position is 1 behind the count of items
